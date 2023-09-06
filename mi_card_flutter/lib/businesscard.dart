b9ccb1b529906d5bd3531f9f0279bc8111836e8b
Roger Jay Sering <rogerjaysering@gmail.com>
2023-09-06 11:51:06 +0800

    finished the challenge

--- mi_card_flutter/lib/businesscard.dart
+++ mi_card_flutter/lib/businesscard.dart
@@ -5,98 +5,81 @@ class bCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 30,
-              height: 30,
-              child: const Image(
-                image: AssetImage("images/diamond.png"),
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("images/diamond.png"),
+            ),
+            Text(
+              "Roger Jay Sering",
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text("Roger Jay Sering",
-            style:  TextStyle(
-              
-            ),
-            ),
-            
-            const SizedBox(
-              height: 10,
-            ),
-            Text("Flutter Developer"),
-            
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 2,
-              width: 500,
-              color: Colors.black,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 10.0),
-                    color: Colors.white,
-                    child: const Center(
-                      child: Image(
-                        image: AssetImage("images/diamond.png"),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    color: Colors.white,
-                    child: const Center(
-                      child: Text(
-                        "+63 0965 220 2568",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
+            Text(
+              "F L U T T E R  D E V E L O P E R",
+              style: TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 10,
+                color: Colors.white,
+                fontFamily: 'Pacifico',
               ),
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 10.0),
-                    color: Colors.white,
-                    child: const Center(
-                      child: Image(
-                        image: AssetImage("images/diamond.png"),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    color: Colors.white,
-                    child: const Center(
-                      child: Text(
-                        "rogerjaysering@gmail.com",
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                ],
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.red,
               ),
             ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.shopping_cart,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+6 965 220 2568",
+                    style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      color: Colors.teal,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "rogerjaysering@gmail.com",
+                    style: TextStyle(
+                      color: Colors.teal,
+                      fontFamily: 'Pacifico',
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
