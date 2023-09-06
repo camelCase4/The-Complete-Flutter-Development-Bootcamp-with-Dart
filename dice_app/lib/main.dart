c6b7b6df98f989d559423e9e9633a2b83f3fc0d8
Roger Jay Sering <rogerjaysering@gmail.com>
2023-09-06 15:03:51 +0800

    finished the dice app challenge and added a counter

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -16,28 +16,88 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  Random rn1 = new Random();
+  Random rn2 = new Random();
+  int theCurrentDie1 = 1;
+  int theCurrentDie2 = 1;
+  int counter = 0;
+  int total = 0;
+  bool clicked = false;
+  String res = "Results";
+
+
+  void test()
+  {
+    theCurrentDie1 = 1 + rn1.nextInt(7 - 1);
+    theCurrentDie2 = 1 + rn2.nextInt(7 - 1);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
+      child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+          Text(
+            "${res}: ${total}",
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset("images/dice1.png"),
+
+          Row(
+           
+            children: [
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    setState(
+                      () {
+                        
+                        /*theCurrentDie1 = 1 + rn1.nextInt(7 - 1);
+                        theCurrentDie2 = 1 + rn2.nextInt(7 - 1);*/
+                        test();
+                        total += theCurrentDie1;
+                        
+                        
+                      },
+                    );
+                  },
+                  child: Image.asset("images/dice${theCurrentDie1}.png"),
+                ),
+              ),
+               Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(
+                  () {
+                    /*theCurrentDie1 = 1 + rn1.nextInt(7 - 1);
+                    theCurrentDie2 = 1 + rn2.nextInt(7 - 1);*/
+                    test();
+
+                    total += theCurrentDie2;
+                  },
+                );
+              },
+              child: Image.asset("images/dice${theCurrentDie2}.png"),
             ),
           ),
+            ],
+          ),
+         
         ],
       ),
     );
   }
 }
+
+
